e0a8bc36f9a5c20049ae601edd8f5c66ad5e2afc
CarlosRevollo <carlos.revollo.villarroel@gmail.com>
2025-09-30 18:03:09 -0400

    base del form restaurante

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,6 @@ android {
     namespace = "com.example.food_point"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
