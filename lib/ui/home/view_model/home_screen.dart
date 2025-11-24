4b81fe5a17c8b0b5dbe92270fb0eec918323a420
CarlosRevollo <carlos.revollo.villarroel@gmail.com>
2025-11-23 22:16:32 -0400

    saul merge

--- lib/ui/home/view_model/home_screen.dart
+++ lib/ui/home/view_model/home_screen.dart
@@ -63,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sabores de mi Tierra'),
+        title: const Text('Sabores de Cochabamba'),
         centerTitle: true,
       ),
       bottomNavigationBar: const CustomBottomNav(selectedIndex: 0),
